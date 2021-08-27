354b6dc36aac14c683fbe1b1ec9fc10fb5de91b2
koinos-ci <koinos-ci@users.noreply.github.com>
2021-08-27 18:33:39 +0000

    Uploading cache info
    Travis:
      TRAVIS_BRANCH: master
      TRAVIS_BUILD_ID: 236397675
      TRAVIS_BUILD_NUMBER: 47
      TRAVIS_JOB_ID: 534266486
      TRAVIS_JOB_NUMBER: 47.2
      TRAVIS_OS_NAME: linux
      TRAVIS_REPO_SLUG: koinos/koinos-cpp-dependencies
    
      Job URL: https://travis-ci.org/koinos/koinos-cpp-dependencies/jobs/534266486

---
+++ 4162dcb/yaml-cpp/0.6.3/f1025e3/a5fbf45/args.cmake
@@ -0,0 +1,4 @@
+set("YAML_CPP_BUILD_TESTS" "OFF" CACHE INTERNAL "")
+set("YAML_CPP_BUILD_TOOLS" "OFF" CACHE INTERNAL "")
+set("CMAKE_CXX_FLAGS" "-fvisibility=hidden" CACHE INTERNAL "")
+set("CMAKE_C_FLAGS" "-fvisibility=hidden" CACHE INTERNAL "")
